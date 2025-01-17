04a4e7a79334e60ec246ce058a7d4398effe63e7
Manav-Pawar <pawar.manav2304@gmail.com>
2025-01-17 13:54:14 +0530

    change db file

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://manav:123@cluster0.3kdrm.mongodb.net/', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect('mongodb+srv://manav:123@cluster0.3kdrm.mongodb.net/');
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection error:', error);
